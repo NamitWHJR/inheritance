e1fd0f8672b8f524a2086c086cd3139a6edab8ff
NamitWHJR <65145516+NamitWHJR@users.noreply.github.com>
2020-05-30 20:09:43 -0700

    Add files via upload

---
+++ Pig.js
@@ -0,0 +1,13 @@
+class Pig extends BaseClass{
+    constructor(x, y) {
+      
+      super(x,y,50,50)
+
+      this.body.density = 1.0
+      
+      this.image = loadImage("sprites/enemy.png")
+      
+    }
+    
+  };
+  
